791a64e329d31709a2716e4f22520c54615dc1dd
Adduq <felipe.quiroga@gmail.com>
2015-10-14 11:25:31 +0200

    global menu

--- test/public/assets/js/globalController.js
+++ test/public/assets/js/globalController.js
@@ -1,9 +1,5 @@
-
 app.controller('globalController', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
-    $rootScope.global_menu = [
-          {title:'SignUp', url:'#/signUp'},
-          {title:'Login', url:'#/login'}
-        ];
+
 	$scope.init = function(){
     // console.log('asdasdadasd');
 	    if (Parse.User.current() != null){
@@ -11,5 +7,24 @@ app.controller('globalController', ['$scope', '$http', '$rootScope', function($s
 	    } else {
 	    	window.location = "#/login";
 	    }
-  	}
+	}
+	if (Parse.User.current() == null){
+		$rootScope.global_menu = [
+			{title:'SignUp', url:'#/signUp'},
+			{title:'Login', url:'#/login'}
+		];
+		window.location = "#/login";
+	} else {
+		$rootScope.global_menu = [
+			{title:'Home', url:'#/home', active: false},
+			{title:'Accounts', url:'#/accounts', active: false},
+			{title:'Transactions', url:'#/transactions', active: false},
+			{title:'Withdrawn', url:'#/withdrawn', active: false},
+			{title:'Transfer', url:'#/transfer', active: false},
+			{title:'Deposit', url:'#/deposit', active: false},
+			{title:'Log Out', url:'#/logOut', active: false }
+		];
+		$scope.user = {};
+		$scope.user.nombre = Parse.User.current().get("username");
+	}
 }]);
