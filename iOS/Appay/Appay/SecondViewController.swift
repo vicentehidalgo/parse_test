3b0147d9198e6e19f5be189b9ebc1329f00a48f7
vicente hidalgo <hidalgosantamaria@gmail.com>
2015-10-19 13:47:09 +0200

    new baseApp yo!

---
+++ iOS/Appay/Appay/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Appay
+//
+//  Created by vicente hidalgo on 16/10/15.
+//  Copyright © 2015 vicente hidalgo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
