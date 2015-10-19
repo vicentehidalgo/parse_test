2b178968267daef3120eee6d0e7ab529baef5707
vicente hidalgo <hidalgosantamaria@gmail.com>
2015-10-19 15:42:53 +0200

    base app gulp

--- baseApp/app/scripts/controllers/accountController.js
+++
@@ -1,8 +0,0 @@
-
-'use strict';
-angular.module('baseApp')
-    .controller('accountController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams) {
-$scope.accountNumber = $routeParams.accountNumber;
-
-
-}]);
---
+++ baseApp/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('baseApp')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
