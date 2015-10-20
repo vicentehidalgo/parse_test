e709d43c36a541858420096aabba7836903cac90
vicente hidalgo <hidalgosantamaria@gmail.com>
2015-10-20 13:45:03 +0200

    digest vs apply

--- baseApp/app/scripts/controllers/accountsController.js
+++ baseApp/app/scripts/controllers/accountsController.js
@@ -17,7 +17,7 @@ angular.module('baseApp')
        vm.accounts[account].balance = vm.accounts[account].get('balance');
      }
      console.log(vm.accounts);
-     $scope.$apply();
+     $scope.$digest();
 
      // Do something with the returned Parse.Object values
      // for (var i = 0; i < results.length; i++) {
