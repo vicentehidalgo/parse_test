1f2864ff8ac516ed330e5d44e53a0a8a8c06ece6
Adduq <felipe.quiroga@gmail.com>
2015-10-20 12:39:19 +0200

    vm in accounts controller

--- baseApp/app/scripts/controllers/accountsController.js
+++ baseApp/app/scripts/controllers/accountsController.js
@@ -2,28 +2,29 @@
 
 angular.module('baseApp')
   .controller('accountsController', ['$scope', '$http', function($scope, $http) {
-  $scope.init = init;
-  $scope.accounts = [];
+  var vm = this;
+  vm.init = init;
+  vm.accounts = [];
 
   function init(){
-
-
-console.log();
-
     var account = Parse.Object.extend("account");
     var query = new Parse.Query(account);
 
     query.containedIn("iban", Parse.User.current().get('accounts'));
-
-
     // ["1001"]
-
     query.find({
       success: function(results) {
         // alert("Successfully retrieved " + results.length);
         console.log(results[0].get('balance'));
-        $scope.accounts = results;
+        vm.accounts = results;
+        for (var account in vm.accounts) {
+          vm.accounts[account].iban = vm.accounts[account].get('iban');
+          vm.accounts[account].name = vm.accounts[account].get('name');
+          vm.accounts[account].balance = vm.accounts[account].get('balance');
+        }
+        console.log(vm.accounts);
         $scope.$apply();
+
         // Do something with the returned Parse.Object values
         // for (var i = 0; i < results.length; i++) {
         //   var object = results[i];
